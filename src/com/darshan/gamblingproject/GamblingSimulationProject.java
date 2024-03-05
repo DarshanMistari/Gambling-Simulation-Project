7d0a9c10ef2112ffbc9a547dec2a65425b91d905
DarshanMistari <mistaridarshan2@gmail.com>
2024-03-05 12:36:07 +0530

    [MoDIFIED]:Darshan - UC-1- Completed Setting up Stack and Bet

--- src/com/darshan/gamblingproject/GamblingSimulationProject.java
+++ src/com/darshan/gamblingproject/GamblingSimulationProject.java
@@ -1,10 +1,21 @@
 package com.darshan.gamblingproject;
 
 public class GamblingSimulationProject {
+	
+	private static final int STACK_PER_DAY = 100;
+	private static final int BET_PER_GAME = 1;
+	
+	public static void gameStatus()
+	{
+		System.out.println("Satck Available :" +STACK_PER_DAY);
+		System.out.println("Bet Per Game :" +BET_PER_GAME);
+		
+	}
 
 	public static void main(String[] args) {
 		
 		System.out.println("Welcome to Gambling Simulation Project Developed by Darshan Mistari");
+	    gameStatus();
 	}
 
 }
