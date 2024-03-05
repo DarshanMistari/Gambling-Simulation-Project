e7e29f2a70d81669803023beac3f1f444d9841bb
DarshanMistari <mistaridarshan2@gmail.com>
2024-03-05 11:54:02 +0530

    [ADDED]:Darshan - Gambling Simulation Project Setup

---
+++ src/com/darshan/gamblingproject/GamblingSimulationProject.java
@@ -0,0 +1,10 @@
+package com.darshan.gamblingproject;
+
+public class GamblingSimulationProject {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Welcome to Gambling Simulation Project Developed by Darshan Mistari");
+	}
+
+}
